17566b946a903140653a7f85ad79b289f3c3835e
Taranpreet Kaur <taranpreet.kaurr04@gmail.com>
2025-07-15 10:03:02 +0530

    Java folder

---
+++ JAVA/src/ID.java
@@ -0,0 +1,7 @@
+public class ID {
+    public static void main(String[] args) {
+        System.out.println("Name :) Taranpreet Kaur");
+        System.out.println("Class :) G1");
+        System.out.println("Roll No. :) 2210990908");
+    }
+}
