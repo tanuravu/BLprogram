484ae4b9cb0be1a58826d93d010417caac32019d
Taranpreet Kaur <taranpreet.kaurr04@gmail.com>
2025-07-15 10:12:02 +0530

    Student ID card program from Scanner input

---
+++ JAVA/src/Card.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Card {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter the name:");
+        String name = sc.nextLine();
+
+        System.out.println("Enter the roll number:");
+        String rollNo = sc.nextLine();
+
+        System.out.println("University Name:");
+        String college = sc.nextLine();
+
+        System.out.println("Branch:");
+        String branch= sc.nextLine();
+
+        // Output
+        System.out.println("\n--- Student Details ---");
+        System.out.println("Name     : " + name);
+        System.out.println("Roll No  : " + rollNo);
+        System.out.println("College  : " + college);
+        System.out.println("Branch Name"+ branch);
+    }
+}
