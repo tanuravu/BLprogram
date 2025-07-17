8d168c707919006b98c3a0b9c1e1c6dc81f96f43
Taranpreet Kaur <taranpreet.kaurr04@gmail.com>
2025-07-17 09:23:26 +0530

    table of 2 program with for and while loop

---
+++ JAVA/src/TableofTwo.java
@@ -0,0 +1,17 @@
+public class TableofTwo {
+    public static void main(String[] args) {
+        //int n=2;
+//        for(int i=1;i<=10;i++){
+//            System.out.println(n+"*"+i+"="+(n*i));
+//        int i=1;
+//        while(i<=10){
+//            System.out.println(n+"*"+i+"="+(n*i));
+//            i++;
+//        }
+            for(int i=0;i<3;i++){
+                for(int j=0;j<4;j++){
+
+            }
+        }
+    }
+}
